1e285078bf7664b175d7e439980a5aaf7a72dcb2
Robel Fetene <ropac15@gmail.com>
2017-10-07 17:59:48 -0400

    adds in profile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const ejs = require('ejs');
 const ejsMate = require('ejs-mate');
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const User = require('./models/user'); 
 
 /*express */
