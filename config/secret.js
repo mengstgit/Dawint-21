a8dc123567cc8df2c5ba1c0412979e4f350f04bd
Mengst bayuh <mengst2014@gmail.com>
2017-10-27 12:53:13 -0400

    added my deployable files to heroku

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-	database: 'mongodb://admin:admin@ds113795.mlab.com:13795/Grill',
+	//database: 'mongodb://admin:admin@ds113795.mlab.com:13795/coffee',
 	port: 3000,
 	secretKey : 'asdf90urqwersv'
 }
