a073dee40dc22ce83320ea3405383279808458f9
Mengst bayuh <mengst2014@gmail.com>
2017-10-28 18:59:17 -0400

    proj files

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-	//database: 'mongodb://admin:admin@ds113795.mlab.com:13795/coffee',
+database: 'mongodb://ethiopia:ethiopia@ds125255.mlab.com:25255/mengst',
 	port: 3000,
 	secretKey : 'asdf90urqwersv'
 }
