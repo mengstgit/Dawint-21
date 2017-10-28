fe65a0891790ed30801e767b11a85412b3b5a2bb
Mengst bayuh <mengst2014@gmail.com>
2017-10-28 19:05:12 -0400

    fixes port

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-database: 'mongodb://ethiopia:ethiopia@ds125255.mlab.com:25255/mengst',
-	port: 3000,
+    database: 'mongodb://ethiopia:ethiopia@ds125255.mlab.com:25255/mengst',
+	port : process.env.PORT || 3000,
 	secretKey : 'asdf90urqwersv'
 }
